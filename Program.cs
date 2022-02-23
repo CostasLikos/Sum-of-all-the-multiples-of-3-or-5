6c97a734ff7df48d11edf4900476d637d8d7a013
Dstrats <james_strats@hotmail.com>
2022-02-23 15:19:10 +0200

    DstratsInit

--- Program.cs
+++ Program.cs
@@ -2,11 +2,15 @@
 
 namespace Solution
 {
-  public static class Program
-  {
-    public static int findSum(int n)
+    public static class Program
     {
-      return Enumerable.Range(1, n).Where(num => num % 3 == 0 || num % 5 == 0).Sum();
+        public static int findSum(int n)
+        {
+            return Enumerable.Range(1, n).Where(num => num % 3 == 0 || num % 5 == 0).Sum();
+        }
+        public static void DenPeirazeiPouDenXerwGit()
+        {
+            System.Console.WriteLine("Matahinw twra");
+        }
     }
-  }
 }
