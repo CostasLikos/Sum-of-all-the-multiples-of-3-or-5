bde8838ff65f9309d930df77a602d900d4b6e478
Costas Likos <costaslikos@gmail.com>
2022-02-23 14:44:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_all_the_multiples_of_3_or_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
