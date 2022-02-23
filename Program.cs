c03d65c70d51d3efe5f4ca6082b0912736f0fc38
Costas Likos <93220708+CostasLikos@users.noreply.github.com>
2022-02-23 15:46:46 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,18 +4,6 @@ namespace Solution
 {
     public static class Program
     {
-        public static int findSum(int n)
-        {
-            return Enumerable.Range(1, n).Where(num => num % 3 == 0 || num % 5 == 0).Sum();
-        }
-        public static void DenPeirazeiPouDenXerwGit()
-        {
-            System.Console.WriteLine("Matahinw twra");
-        }
-       public static int FindSum2(int n)
-        {
-            return Enumerable.Range(1, n).Where(num => num % 2 == 0).Sum();
-        }
-
+        public static int findSum(int n) => Enumerable.Range(1, n).Where(num => num % 3 == 0 || num % 5 == 0).Sum();
     }
 }
